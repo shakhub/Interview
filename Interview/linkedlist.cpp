f6cdb3c22b29e25a78d083c4f25602718fc0237d
ShankOffice <sshivakumar@pc17949.iteris.com>
2015-06-05 12:22:15 -0700

    Converted linkedlist to c++ classes

--- Interview/linkedlist.cpp
+++ Interview/linkedlist.cpp
@@ -1,7 +1,9 @@
 #include<stdlib.h>
 #include<stdio.h>
+#include<iostream>
 #include"linkedlist.hpp"
-#if 1
+using namespace std;
+#if 0
 LinkedList *createList(int data)
 {
 	LinkedList *head = (LinkedList *)malloc(sizeof(LinkedList));
@@ -120,31 +122,226 @@ void runLinkedList(void)
 LinkedList::LinkedList()
 {
 	//constructor
+	head = tail = NULL;
 	
 }
-LinkedList::LinkedList(int data)
+LinkedList::LinkedList(int value)
 {
 	//constructor
-	LinkedList *head = (LinkedList *)malloc(sizeof(LinkedList));
-	head->data = data;
-	head->next = NULL;
+	head = new Node(value);
+	tail = head;
 }
-LinkedList *LinkedList::createNode(int data)
+LinkedList::~LinkedList()
 {
-	LinkedList *head = new LinkedList;
-	return head;
+	deleteList();
 }
-void LinkedList::push(int data)
+void LinkedList::deleteList()
+{
+	if(isEmpty()) {cout<< "Empty list"<<endl; return;}
+	Node *curr = head;
+	Node *temp;
+
+	while(curr!=NULL)
+	{
+		temp = curr;
+		curr = curr->next;
+		delete temp;
+	}
+	head = tail = NULL;
+	cout << "List deleted"<<endl;
+}
+void LinkedList::push(int value)
 {
 	//add a node to the head of a linked list
-	LinkedList *newNode = this;
-	newNode->next = *node; // new node will point to where the head used to point
-	*node = newNode;//move the head to point to the new node
+	if(head == NULL)
+		tail = head = new Node(value);
+
+	Node * newNode = new Node(value);
+	newNode->next = head;
+	head = newNode; //update the head node
+
+}
+void LinkedList::push(Node *node,int value)
+{
+	//push after given node with the given data
+	if(node!=NULL)
+		node->next = new Node(value,node->next);
+
+}
+void LinkedList::push_at_tail(int value)
+{
+	if(head == NULL)
+		tail = head = new Node(value);
+
+	tail->next = new Node(value);
+	tail = tail->next;
+	
+}
+int LinkedList::length()
+{
+	if(isEmpty()) {cout << " Empty list "<<endl;return 0;}
+	Node *curr = head;
+	int len=0;
+	while(curr != NULL)
+	{
+		curr = curr->next;
+		len++;
+	}
+	cout << "Length of list : "<<len<<endl;
+	return len;
+}
+int LinkedList::search(int value)
+{
+	if(isEmpty()){cout << " Empty list "<<endl; return -1;}
+	Node *curr = head;
+	int loc=1;
+	while(curr!=NULL)
+	{
+		if(curr->data == value)
+		{
+			cout<< "Data "<<value<<" found at location :"<<loc<<endl;
+			return loc;
+		}
+		loc++;
+		curr = curr->next;
+	}
+	cout<< "Data "<< value<<" not found in the list"<<endl;
+	return -1;
+
+}
+int LinkedList::pop()
+{
+	//removes node at the head;
+	int data;
+	if(isEmpty()){cout << "Cannot pop : Empty list"<<endl; return -1;}
+	
+	if(tail == head) // only one node in the list
+	{
+
+	}
+	Node *temp = head;
+	data = temp->data;
+	head = head->next;
+	delete temp;
+	return data;
+
+}
+int LinkedList::pop_tail()
+{
+	//removes node at the tail;
+	Node *prev=NULL,*curr=NULL;
+	int data;
+	if(isEmpty()){cout << "Cannot pop : Empty list"<<endl; return -1;}
+	if(tail == head) // only one node in the list
+	{
+
+	}
+	curr = head;
+	while(curr->next!=NULL)
+	{
+		prev = curr;
+		curr= curr->next;
+	}
+	prev->next = NULL;
+	tail = prev;
+	data = curr->data;
+	delete curr;
+	return data;
+}
+int LinkedList::get_node_at_loc(int idx)
+{
+	// gets the data stored at location idx from the head
+	if(isEmpty()){cout << "Empty list: Nothing to get."<<endl; return -1;}
+	
+	if(idx > length())
+	{
+		cout<<"Invalid index: List is smaller than the index"<<endl;
+		return -1;
+	}
+
+	Node *curr = head;
+	int count=1;
+	while(count!=idx && curr!=NULL)
+	{
+		curr = curr->next;
+		count++;
+	}
+	
+	cout << "Data at location "<<idx<<": "<<curr->data<<endl;
+	return curr->data;
+}
+void LinkedList::delete_node(int value)
+{
+	Node *prev=NULL,*temp=NULL;
+	if(head->data == value)
+	{
+		temp = head;
+		head = head->next;
+		delete temp;
+		return;
+	}
+	prev = head;
+	temp = head->next;
+	while(temp!=NULL)
+	{
+		if(temp->data == value)
+		{
+			prev->next = temp->next;
+			if(temp == tail)
+			{
+				tail = prev;
+			}
+			delete temp;
+			break;
+		}
+		prev = temp;
+		temp = temp->next;
+	}
+
+}
+void LinkedList::print_list()
+{
+	Node *curr = head;
+
+	if(isEmpty()){cout << "Empty list"<<endl; return;}
+	while(curr!=NULL)
+	{
+		cout<< curr->data<<" ";
+		curr=curr->next;
+	}
+	cout<<endl;
+}
+void LinkedList::reverse()
+{
+	if(head == NULL)
+	{
+		cout << "Empty list : Cannot reverse"<<endl;
+		return;
+	}
+	Node *curr,*next,*prev = NULL;
+
+	curr = head;
+	tail = curr;
+	if(curr == NULL) return;
+	while(curr != NULL)
+	{
+		next = curr->next;
+		curr->next = prev;
+		prev = curr;
+		curr = next;
+	}
+	head = prev;
+	tail->next = NULL; //  not necessary just for safety
 
 }
 void runLinkedList()
 {
-	LinkedList list();
-
+	LinkedList list;
+	list.push(4);
+	list.push(5);
+	list.push(6);
+	list.push(7);
+	list.print_list();	
+	
 }
 #endif
--- Interview/linkedlist.hpp
+++ Interview/linkedlist.hpp
@@ -1,4 +1,4 @@
-
+/*
 typedef struct LinkedList
 {
 	int data;
@@ -13,26 +13,60 @@ int popLinkList(LinkedList **);
 int removeLastNodeLinkList(LinkedList **);
 void deleteNode(LinkedList **, int );
 void printList(LinkedList **);
-void runLinkedList(void);
 
+*/
+class Node
+{
+	friend class LinkedList;
+private:
+	int data;
+	Node *next;
+public:
+	//constructor
+	Node(void)
+		:next(NULL)
+	{}
+	//constructor with argument
+	Node(int value)
+		:next(NULL),data(value)
+	{}
+	Node(int value,Node *next)
+		:next(next),data(value)
+	{}
+	int getData(void) { return data;}		
+	Node* getNext(void) { return next;}		
+};
 
-/*
 class LinkedList
 {
-	LinkedList *next;
-	int data;
+private:
+	Node *head;
+	Node *tail;
 public:
-	LinkedList (); //constructor
-	LinkedList (int value =0):next(NULL),data(value){}; //constructor
-	LinkedList * createNode(int data);
-	void push(int data);
-	void push(LinkedList *node);
-	void push_at_end(int data);
+	LinkedList (void); //constructor
+	LinkedList (int value);//constructor
+	~LinkedList(void);//distructor
+	bool isEmpty()
+	{
+		if(head == NULL) 
+		{
+			return true;
+		}
+		return false;
+	}
+	int length();
+	int search(int value); // returns the location of the value if found
+	void deleteList();
+	void push(int value);
+	void push(Node *node,int value);// push after given node
+	void push_at_tail(int value);
 	int pop();
-	int pop_at_end();
-	void delete_node(int data);
+	int pop_tail();
+	int get_node_at_loc(int location);
+	void delete_node(int value);
 	void print_list();
 
+	void reverse();// reverse the linked list
 
 };
-*/
+void runLinkedList(void);
