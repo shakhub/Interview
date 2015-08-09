ccb33b941969c971bb26b0833220dd901b1829ce
Shank Home <jayarams@usc.edu>
2015-08-09 13:09:21 -0700

    use of vector stl

--- Interview/graph.cpp
+++ Interview/graph.cpp
@@ -3,22 +3,27 @@
 
 
 #define __GRAPH
-#define SIZE 4
+#define SIZE 6
 //char vertex[8] = {'A','B','C','D','E','F','G','H'};
 
 bool Graph::loop()
 {
+	// Detects cycle in the given graph 
+	// uses DFS and extra stack to keep track of 
+	// the visited vetices 
+
 	return false;
 }
 void Graph::dfs_util(int start,bool visited[])
 {
+	
 	visited[start] = true;
 	std::cout << start << " ";
-
 	Node<int> *curr = array[start].gethead();
 
 	while(curr!=NULL)
 	{
+		
 		if(!visited[curr->getData()])
 			dfs_util(curr->getData(),visited);
 		curr = curr->getNext();
@@ -29,9 +34,11 @@ void Graph::dfs()
 {	
 	bool visited[SIZE] = {false};
 	std::cout<<std::endl;
-	for (int i=0;i<size;i++)
-		if(!visited[i])
-			dfs_util(i,visited);
+	for (int i = 0; i < size; i++)
+	{
+		if (!visited[i])
+			dfs_util(i, visited);
+	}
 	std::cout<<std::endl;
 }
 void Graph::bfs(int start)
@@ -98,6 +105,33 @@ void Graph::path(int start,int end)
 	
 
 }
+void Graph::topologicalsort_util(int start,bool visited[],Stack<int> *s)
+{
+	Node<int> *curr = array[start].gethead();
+	visited[start] = true;
+	while (curr != NULL)
+	{
+		if (!visited[curr->getData()])
+			topologicalsort_util(curr->getData(), visited, s);
+	}
+	s->push(start);
+	
+}
+void Graph::topologicalsort()
+{
+	Stack<int> s(SIZE);
+	bool visited[SIZE] = { false };
+
+	for (int i = 0; i < SIZE; i++)
+	{
+		if (!visited[i])
+			topologicalsort_util(i, visited, &s);
+	}
+	while (!s.isEmpty())
+		std::cout << s.pop() << " ";
+	std::cout << std::endl;
+
+}
 void Graph::printgraph()
 {
 	for(int i=0;i<size;i++)
@@ -111,14 +145,22 @@ void runGraph()
 {
 	Graph g(SIZE);
 	
-	
+	/*
 	g.addedge_d(0, 1);
     g.addedge_d(0, 2);
     g.addedge_d(1, 2);
     g.addedge_d(2, 0);
     g.addedge_d(2, 3);
-    g.addedge_d(3, 3);
+    g.addedge_d(3, 3);*/
 	
+	g.addedge_d(5, 2);
+	g.addedge_d(5, 0);	
+	g.addedge_d(4, 0);
+	g.addedge_d(4, 1);
+	g.addedge_d(2, 3);
+	g.addedge_d(3, 1);
+	
+		
 	/*
 	g.addedge(0,1);
     g.addedge(0,6);
@@ -131,8 +173,8 @@ void runGraph()
 	g.addedge(4,6);*/
 	
 	g.printgraph();
-	g.dfs();
-	g.path(3,3);
+	//g.dfs();
+	g.topologicalsort();
 	/*
 	if(g.loop())
 		std::cout<<"Graph has cycle"<<std::endl;
--- Interview/graph.hpp
+++ Interview/graph.hpp
@@ -1,5 +1,6 @@
 #ifndef __GRAPH__H
 #define __GRAPH__H
+#include<vector>
 #include"linkedlist.hpp"
 #include"stacks.hpp"
 #include"queue.hpp"
@@ -12,16 +13,20 @@ class Graph
 	*/
 	int size;
 	LinkedList<int> *array; //array of linked lists
+	
 public:
 	Graph(int size){ array = new LinkedList<int>[size]; this->size = size;}
-	~Graph(){delete []array;}
-	void addedge(int src,int dest)
-	{
-		array[src].push(dest);
-		array[dest].push(src); // since its an undirected graph};
+	~Graph(){delete []array;}	
 
-		array[src].sort();
-		array[dest].sort();
+	void addedge(int start,int end)
+	{
+
+		array[start].push(end);
+		array[end].push(start); // since its an undirected graph};
+
+		array[start].sort();
+		array[end].sort();
+
 	}
 	void addedge_d(int src,int dest) {array[src].push(dest);} // directed graphs
 	void dfs(); // vertext start point
@@ -29,6 +34,8 @@ public:
 	void bfs(int start);
 	void dfs_path(int start, int end,bool visited[],Stack<int> *list);
 	void path(int start,int end);
+	void topologicalsort_util(int start, bool visited[], Stack<int> *s);
+	void topologicalsort();
 	bool loop();
 	void printgraph();
 };
